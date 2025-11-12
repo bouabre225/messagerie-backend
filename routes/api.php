4508293abe61baa9bd72475f5b73530be2934b08
bouabre225 <angebouabre463@gmail.com>
2025-11-12 18:38:40 +0100

    ajout des routes de connexions, de deconnexion et celui de l'utilisateur

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+//route protegé
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/me', [AuthController::class, 'me']);
+});
 
